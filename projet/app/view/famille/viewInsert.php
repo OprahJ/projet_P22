9fcd521b123f386c53a63440fa2aaa377b007a16
Utilisateur <Utilisateur@10.95.2.76>
2022-06-09 23:34:00 +0200

    Mise à jour 09/06

--- projet/app/view/famille/viewInsert.php
+++ projet/app/view/famille/viewInsert.php
@@ -1,30 +1,31 @@
 
 <!-- ----- début viewInsert -->
- 
-<?php 
+
+<?php
 require ($root . '/app/view/fragment/fragmentGeneHeader.html');
 ?>
 
 <body>
-  <div class="container">
-    <?php
-      include $root . '/app/view/fragment/fragmentGeneMenu.html';
-      include $root . '/app/view/fragment/fragmentGeneJumbotron.php';
-    ?> 
+    <div class="container">
+        <?php
+        include $root . '/app/view/fragment/fragmentGeneMenu.html';
+        include $root . '/app/view/fragment/fragmentGeneJumbotron.php';
+        ?> 
 
-    <form role="form" method='get' action='router1.php'>
-      <div class="form-group">
-        <input type="hidden" name='action' value='famCreated'>        
-        <label for="id">nom : </label><input type="text" name='nom' size='75' value=''>                           
-        
-      <p/>
-      <button class="btn btn-primary" type="submit">Go</button>
-    </form>
-    <p/>
-  </div>
-  <?php include $root . '/app/view/fragment/fragmentGeneFooter.html'; ?>
+        <form role="form" method='get' action='router1.php'>
+            <div class="form-group">
+                <input type="hidden" name='action' value='famCreated'>        
+                <label for="id">nom : </label><input type="text" name='nom' size='75' value=''>                           
 
-<!-- ----- fin viewInsert -->
+                <p/>
+                <button class="btn btn-primary" type="submit">Go</button>
+            </div>
+        </form>
+        <p/>
+    </div>
+    <?php include $root . '/app/view/fragment/fragmentGeneFooter.html'; ?>
+
+    <!-- ----- fin viewInsert -->
 
 
 
